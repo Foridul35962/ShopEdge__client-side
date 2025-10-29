f15dfdfa328a4386ac460d626df31aac96c868bc
Foridul35962 <foridulislam35962@gmail.com>
2025-10-30 00:38:29 +0600

    Fix filename casing

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { FaUserCircle, FaShoppingBag } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom'
+import Searchbar from './Searchbar';
 
 const Navbar = () => {
     const navigate = useNavigate()
     return (
         <div className='bg-[#e5c185]'>
             <div className='container mx-auto flex justify-between items-center p-2'>
-                <div onClick={()=>navigate('/')} className='cursor-pointer text-2xl'>
+                <div onClick={() => navigate('/')} className='cursor-pointer text-2xl'>
                     ShopEdge
                 </div>
                 <div className='flex gap-5 *:cursor-pointer'>
@@ -16,11 +17,14 @@ const Navbar = () => {
                     <p>TOP WEAR</p>
                     <p>BOTTUM WEAR</p>
                 </div>
-                <div className='flex gap-4 text-2xl'>
-                    <FaUserCircle />
+                <div className='flex gap-4 items-center text-2xl'>
+                    <FaUserCircle className='cursor-pointer' />
                     <div className='relative'>
-                        <FaShoppingBag/>
-                        <p className='absolute px-1 -top-3 -right-2 text-lg bg-red-700 rounded-full'>0</p>
+                        <FaShoppingBag className='cursor-pointer' />
+                        <p className='absolute px-1 -top-2 -right-2 text-[16px] bg-red-700 rounded-full'>0</p>
+                    </div>
+                    <div className='overflow-hidden'>
+                        <Searchbar />
                     </div>
                 </div>
             </div>
--- src/components/common/Searchbar.jsx
+++ src/components/common/Searchbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaSearch } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Searchbar = () => {
+  const [searchItem, setSearchItem] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div>Searchbar</div>
+    <div className={`flex justify-center items-center transform transition-all duration-300 ${isOpen ? 'absolute top-0 left-0 z-50 bg-white h-22 dark:bg-gray-800 w-full' : 'w-auto'}`}>
+      {
+        isOpen ? (
+          <form onSubmit={handleSubmit} className='relative flex items-center justify-center w-full'>
+            <div className='relative w-1/2'>
+              <input type="text" name="search" placeholder='Search Here' value={searchItem} className='w-full bg-gray-300 px-4 py-2 pl-2 pr-12 text-xl rounded-lg focus:outline-none placeholder:text-gray-700' onChange={(e)=>setSearchItem(e.target.value)} onKeyDown={(e)=>e.key==='Enter' && handleSubmit()} />
+              <button type='submit' className='cursor-pointer absolute right-1 top-[30%]'>
+                <FaSearch />
+              </button>
+            </div>
+            <AiOutlineClose className='cursor-pointer absolute right-3 top-[27%] text-black dark:text-white' onClick={()=>setIsOpen(!isOpen)} />
+          </form>
+        ) : (
+          <button>
+            <FaSearch className='cursor-pointer' onClick={()=>setIsOpen(!isOpen)} />
+          </button>
+        )
+      }
+    </div>
   )
 }
 
