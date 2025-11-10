fad2d6ac50b78b7c442c77d0a1350f22779988b6
Foridul35962 <foridulislam35962@gmail.com>
2025-11-11 01:37:02 +0600

    fixed table scroll style in small device

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -56,9 +56,9 @@ const OrderDetails = () => {
                             <h3 className='font-bold'>Order ID: #{orderDetail._id}</h3>
                             <p>{orderDetail.createdAt.toLocaleDateString()}</p>
                         </div>
-                        <div className='flex flex-col gap-2'>
-                            <p className='bg-green-300 text-center px-2 py-1 rounded-xl'>Approved</p>
-                            <p className={`px-2 py-1 rounded-xl ${orderDetail.isDelivered ? 'bg-green-400' : 'bg-amber-200'}`}>{orderDetail.isDelivered ? 'Delivered' : 'Pending Delivery'}</p>
+                        <div className='flex flex-col sm:items-end gap-2'>
+                            <p className='bg-green-300 w-fit px-2 py-1 rounded-xl'>Approved</p>
+                            <p className={`px-2 py-1 w-fit rounded-xl ${orderDetail.isDelivered ? 'bg-green-400' : 'bg-amber-200'}`}>{orderDetail.isDelivered ? 'Delivered' : 'Pending Delivery'}</p>
                         </div>
                     </div>
                     <div className='flex flex-col sm:flex-row gap-2 sm:gap-30'>
@@ -79,40 +79,47 @@ const OrderDetails = () => {
                     </div>
                     <div className='flex flex-col gap-2'>
                         <h3 className='font-bold'>Products</h3>
-                        <table className="min-w-full border border-gray-200 rounded-xl overflow-hidden shadow-md">
-                            <thead className="bg-gray-100">
-                                <tr>
-                                    <th className="py-3 px-4 text-left font-semibold text-gray-700">Image</th>
-                                    <th className="py-3 px-4 text-left font-semibold text-gray-700">Name</th>
-                                    <th className="py-3 px-4 text-left font-semibold text-gray-700">Unit Price</th>
-                                    <th className="py-3 px-4 text-left font-semibold text-gray-700">Quantity</th>
-                                    <th className="py-3 px-4 text-left font-semibold text-gray-700">Total</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                {orderDetail.orderItems.map((item, idx) => (
-                                    <tr
-                                        key={idx}
-                                        className="border-t border-gray-200 odd:bg-white even:bg-gray-50 hover:bg-gray-100 transition"
-                                    >
-                                        <td className="py-3 px-4">
-                                            <img
-                                                src={item.image}
-                                                alt="product"
-                                                className="size-12 object-cover rounded-md border"
-                                            />
-                                        </td>
-                                        <td onClick={()=>navigate(`/product/${item.productId}`)} className="py-3 px-4 font-medium text-gray-800 cursor-pointer hover:underline">{item.name}</td>
-                                        <td className="py-3 px-4 text-gray-700">${item.price}</td>
-                                        <td className="py-3 px-4 text-gray-700">{item.quantity}</td>
-                                        <td className="py-3 px-4 font-semibold text-gray-900">
-                                            ${item.price * item.quantity}
-                                        </td>
+                        <div className="w-full overflow-x-auto">
+                            <table className="min-w-max w-full border border-gray-200 rounded-xl shadow-md">
+                                <thead className="bg-gray-100">
+                                    <tr>
+                                        <th className="py-3 px-4 text-left font-semibold text-gray-700">Image</th>
+                                        <th className="py-3 px-4 text-left font-semibold text-gray-700">Name</th>
+                                        <th className="py-3 px-4 text-left font-semibold text-gray-700">Unit Price</th>
+                                        <th className="py-3 px-4 text-left font-semibold text-gray-700">Quantity</th>
+                                        <th className="py-3 px-4 text-left font-semibold text-gray-700">Total</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+
+                                <tbody>
+                                    {orderDetail.orderItems.map((item, idx) => (
+                                        <tr
+                                            key={idx}
+                                            className="border-t border-gray-200 odd:bg-white even:bg-gray-50 hover:bg-gray-100 transition"
+                                        >
+                                            <td className="py-3 px-4">
+                                                <img
+                                                    src={item.image}
+                                                    alt="product"
+                                                    className="size-12 object-cover rounded-md border"
+                                                />
+                                            </td>
+                                            <td
+                                                onClick={() => navigate(`/product/${item.productId}`)}
+                                                className="py-3 px-4 font-medium text-gray-800 cursor-pointer hover:underline"
+                                            >
+                                                {item.name}
+                                            </td>
+                                            <td className="py-3 px-4 text-gray-700">${item.price}</td>
+                                            <td className="py-3 px-4 text-gray-700">{item.quantity}</td>
+                                            <td className="py-3 px-4 font-semibold text-gray-900">
+                                                ${item.price * item.quantity}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                         <h4
                             onClick={() => navigate('/orders')}
                             className='underline text-blue-700 cursor-pointer'>Back to My Order</h4>
