291e5c74416047e765cce90b7b9f7373be876d35
Foridul35962 <foridulislam35962@gmail.com>
2025-11-12 02:02:27 +0600

    fixed hight in navbar

--- src/pages/AdminLayout.jsx
+++ src/pages/AdminLayout.jsx
@@ -17,7 +17,7 @@ const AdminLayout = () => {
       </div>
       <div className='flex gap-3 w-full'>
         <div
-          className={`bg-gray-800 text-white h-screen z-50 w-64 absolute sm:relative transform
+          className={`bg-gray-800 text-white z-50 w-64 absolute sm:relative transform
         ${sidebar ? "translate-x-0" : "-translate-x-full"} transition-all duration-300 sm:translate-x-0`}>
           <AdminSidebar setSidebar={setSidebar} />
         </div>
